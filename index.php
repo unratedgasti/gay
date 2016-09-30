f7f876dfd000ed8ff24724811f1f75b8f0f739cd
unratedgasti <danieltinao@gmail.com>
2016-09-30 16:33:16 +0800

    abc

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
   <title>Mr. Gay Philippines</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="js/jquery-1.12.4.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
   <style>
     .carousel-inner > .item > img,
     .carousel-inner > .item > a > img {
@@ -74,7 +74,7 @@
               if($row1['judgcode'] != 12){
                  echo '             
               <tr >
-              <td width="23%"></td>
+              <td width="21%"></td>
               <td > <h3 > JUDGE '.$var.'</h3></td>
               <td width="60%"><h3>-</h3></td>
               <td><h3>'. $score .'</h3> </td>
